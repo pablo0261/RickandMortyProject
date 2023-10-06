2e6b802bc19930bc3b8b23f94219e627d6501ef7
pablo0261 <pablobesler@gmail.com>
2023-10-06 00:20:09 -0300

    prueba

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./Redux/store.js";
 import { Provider } from "react-redux";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
